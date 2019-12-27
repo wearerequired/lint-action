cd96e8d31c637c88d8ea2786d7c61cb5957132e2
Samuel Meuli <me@samuelmeuli.com>
2019-12-27 19:47:26 +0100

    Run formatters after linters

--- src/linters/index.js
+++ src/linters/index.js
@@ -7,13 +7,16 @@ const Stylelint = require("./stylelint");
 const SwiftLint = require("./swiftlint");
 
 const linters = {
-	black: Black,
+	// Linters
 	eslint: ESLint,
 	flake8: Flake8,
-	gofmt: Gofmt,
-	prettier: Prettier,
 	stylelint: Stylelint,
 	swiftlint: SwiftLint,
+
+	// Formatters (should be run after linters)
+	black: Black,
+	gofmt: Gofmt,
+	prettier: Prettier,
 };
 
 module.exports = linters;
