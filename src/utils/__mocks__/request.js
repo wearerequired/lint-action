717be6ec3ee7779468240394e7b9f46f9ffab0fc
Samuel Meuli <me@samuelmeuli.com>
2020-01-07 13:35:43 +0100

    Add TODO for mock dir location

--- src/utils/__mocks__/request.js
+++ src/utils/__mocks__/request.js
@@ -1,3 +1,6 @@
+// TODO: Move `__mocks__` into `test` directory once supported by Jest
+// https://github.com/facebook/jest/issues/2726
+
 const checkRunsResponse = require("./responses/check-runs.json");
 
 const CHECK_RUNS_REGEX = /^https:\/\/api\.github\.com\/repos\/.*\/.*\/check-runs$/;
