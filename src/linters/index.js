a07b07a7a95372720ba1b6f53785184f9e6a0187
Samuel Meuli <me@samuelmeuli.com>
2020-01-08 20:28:17 +0100

    Add missing `golint` import

--- src/linters/index.js
+++ src/linters/index.js
@@ -2,6 +2,7 @@ const Black = require("./black");
 const ESLint = require("./eslint");
 const Flake8 = require("./flake8");
 const Gofmt = require("./gofmt");
+const Golint = require("./golint");
 const Prettier = require("./prettier");
 const RuboCop = require("./rubocop");
 const Stylelint = require("./stylelint");
@@ -12,6 +13,7 @@ const linters = {
 	// Linters
 	eslint: ESLint,
 	flake8: Flake8,
+	golint: Golint,
 	rubocop: RuboCop,
 	stylelint: Stylelint,
 	swiftlint: SwiftLint,
