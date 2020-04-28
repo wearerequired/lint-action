6afd006c4955be14709c45cd34be66854972f29d
Florian Harr <florian.harr@wsj.com>
2020-04-28 18:59:39 -0400

    Swiftlint dir

--- src/linters/swiftformat.js
+++ src/linters/swiftformat.js
@@ -39,7 +39,7 @@ class SwiftFormat {
 		}
 
 		const fixArg = fix ? "" : "--lint";
-		return run(`${prefix} swiftformat ${fixArg} ${args} "."`, {
+		return run(`${prefix} swiftformat ${fixArg} ${args}`, {
 			dir,
 			ignoreErrors: true,
 		});
