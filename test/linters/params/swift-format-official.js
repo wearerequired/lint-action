c2bf9467d747c01deae1a6cb57da31e9de446a96
Saleem Abdulrasool <compnerd@compnerd.org>
2021-05-09 14:43:58 -0700

    test: stylistic change for recording the warning (NFC)
    This removes the trailing `,` to allow the warnings to be listed
    similarly.  This is purely a stylistic change intended to help simplify
    future changes.

--- test/linters/params/swift-format-official.js
+++ test/linters/params/swift-format-official.js
@@ -10,10 +10,7 @@ const extensions = ["swift"];
 function getLintParams(dir) {
 	const warning1 = `${join(dir, "file2.swift")}:2:22: warning: [DoNotUseSemicolons]: remove ';'`;
 	const warning2 = `${join(dir, "file1.swift")}:3:35: warning: [RemoveLine]: remove line break`;
-	const warning3 = `${join(
-		dir,
-		"file1.swift",
-	)}:7:1: warning: [Indentation] replace leading whitespace with 2 spaces`;
+	const warning3 = `${join(dir, "file1.swift")}:7:1: warning: [Indentation] replace leading whitespace with 2 spaces`;
 	const warning4 = `${join(dir, "file1.swift")}:7:23: warning: [Spacing]: add 1 space`;
 	// Files on macOS are not sorted.
 	const stderr =
