d56f5fa6b6bd309fed6dfda9e63346bfff3ae132
Ryan Marks <ryan@seamless.md>
2021-12-29 11:36:27 -0500

    Remove default . arg

--- src/linters/eslint.js
+++ src/linters/eslint.js
@@ -48,7 +48,7 @@ class ESLint {
 		const fixArg = fix ? "--fix" : "";
 		const commandPrefix = prefix || getNpmBinCommand(dir);
 		return run(
-			`${commandPrefix} eslint --ext ${extensionsArg} ${fixArg} --no-color --format json ${args} "."`,
+			`${commandPrefix} eslint --ext ${extensionsArg} ${fixArg} --no-color --format json ${args ? args :  '"."'}`,
 			{
 				dir,
 				ignoreErrors: true,
