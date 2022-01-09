d3c711467d285033115111956250ac4068101e62
Dominik Schilling <dominikschilling+git@gmail.com>
2022-01-09 18:44:02 +0100

    Format files

--- test/mock-actions-core.js
+++ test/mock-actions-core.js
@@ -2,12 +2,12 @@
 jest.mock("@actions/core", () => {
 	const original = jest.requireActual("@actions/core");
 	return {
-			...original,
-			debug: jest.fn(),
-			error: jest.fn(),
-			warning: jest.fn(),
-			info: jest.fn(),
-			startGroup: jest.fn(),
-			endGroup: jest.fn(),
+		...original,
+		debug: jest.fn(),
+		error: jest.fn(),
+		warning: jest.fn(),
+		info: jest.fn(),
+		startGroup: jest.fn(),
+		endGroup: jest.fn(),
 	};
 });
