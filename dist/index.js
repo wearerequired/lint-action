7a6b720d31a663cf2e8831603a4accb4ce536a96
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2022-04-13 19:32:18 +0300

    Bump build

--- dist/index.js
+++ dist/index.js
@@ -4317,7 +4317,7 @@ const { getSummary } = __nccwpck_require__(9149);
 async function runAction() {
 	const context = getContext();
 	const autoFix = core.getInput("auto_fix") === "true";
-	const autoCommit = core.getInput("auto_commit") === "true";
+	const commit = core.getInput("commit") === "true";
 	const skipVerification = core.getInput("git_no_verify") === "true";
 	const continueOnError = core.getInput("continue_on_error") === "true";
 	const gitName = core.getInput("git_name", { required: true });
@@ -4401,7 +4401,7 @@ async function runAction() {
 				hasFailures = true;
 			}
 
-			if (autoFix && autoCommit) {
+			if (autoFix && commit) {
 				// Commit and push auto-fix changes
 				if (git.hasChanges()) {
 					git.commitChanges(commitMessage.replace(/\${linter}/g, linter.name), skipVerification);
