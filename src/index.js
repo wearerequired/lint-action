9fc05981388d86bcb84942a4ebf44054bc15876c
Bastien Faivre <faivre.bast@gmail.com>
2022-04-14 20:58:32 +0200

    fix: update the code after PR comments

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ async function runAction() {
 			const lintDirRel = core.getInput(`${linterId}_dir`) || ".";
 			const prefix = core.getInput(`${linterId}_command_prefix`);
 			const lintDirAbs = join(context.workspace, lintDirRel);
-			const linterAutoFix = core.getInput(`${linterId}_auto_fix`) === "true";
+			const linterAutoFix = false === autoFix ?  false : core.getInput(`${linterId}_auto_fix`) === "true";
 
 			// Check that the linter and its dependencies are installed
 			core.info(`Verifying setup for ${linter.name}…`);
@@ -82,14 +82,14 @@ async function runAction() {
 
 			// Lint and optionally auto-fix the matching files, parse code style violations
 			core.info(
-				`Linting ${autoFix && linterAutoFix ? "and auto-fixing " : ""}files in ${lintDirAbs} ` +
+				`Linting ${linterAutoFix ? "and auto-fixing " : ""}files in ${lintDirAbs} ` +
 					`with ${linter.name} ${args ? `and args: ${args}` : ""}…`,
 			);
 			const lintOutput = linter.lint(
 				lintDirAbs,
 				fileExtList,
 				args,
-				autoFix && linterAutoFix,
+				linterAutoFix,
 				prefix,
 			);
 
@@ -104,7 +104,7 @@ async function runAction() {
 				hasFailures = true;
 			}
 
-			if (autoFix && linterAutoFix) {
+			if (linterAutoFix) {
 				// Commit and push auto-fix changes
 				if (git.hasChanges()) {
 					git.commitChanges(commitMessage.replace(/\${linter}/g, linter.name), skipVerification);
