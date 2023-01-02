b522f086c9fa038df98b8cb6bf4cb6273ec8aca3
Dominik Schilling <dominikschilling+git@gmail.com>
2023-01-02 21:25:25 +0100

    Update Go version used for tests (#561)
    * Update Go version used for tests
    
    * Update expected output for gofmt tests
    
    * Update diff format for new gofmt version

--- src/linters/gofmt.js
+++ src/linters/gofmt.js
@@ -71,7 +71,7 @@ class Gofmt {
 		// from the gofmt diff so the diff parser can read the diff without errors
 		const filteredOutput = output.stdout
 			.split(/\r?\n/)
-			.filter((line) => !line.startsWith("diff -u"))
+			.filter((line) => !line.startsWith("diff "))
 			.join("\n");
 		lintResult.error = parseErrorsFromDiff(filteredOutput);
 
