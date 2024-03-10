c41e029f653b39d1f7ad04383d0acd0f87059011
Grégoire Mat <65409906+itsvyle@users.noreply.github.com>
2024-03-10 23:32:43 +0000

    Added staticcheck to actual linter

--- src/linters/index.js
+++ src/linters/index.js
@@ -19,40 +19,42 @@ const Stylelint = require("./stylelint");
 const SwiftFormatLockwood = require("./swift-format-lockwood");
 const SwiftFormatOfficial = require("./swift-format-official");
 const SwiftLint = require("./swiftlint");
+const StaticCheck = require("./staticcheck");
 const TSC = require("./tsc");
 const XO = require("./xo");
 
 const linters = {
-	// Linters
-	clippy: Clippy,
-	erblint: Erblint,
-	eslint: ESLint,
-	flake8: Flake8,
-	golint: Golint,
-	mypy: Mypy,
-	php_codesniffer: PHPCodeSniffer,
-	pylint: Pylint,
-	rubocop: RuboCop,
-	stylelint: Stylelint,
-	swiftlint: SwiftLint,
-	xo: XO,
-	tsc: TSC,
+  // Linters
+  clippy: Clippy,
+  erblint: Erblint,
+  eslint: ESLint,
+  flake8: Flake8,
+  golint: Golint,
+  mypy: Mypy,
+  php_codesniffer: PHPCodeSniffer,
+  pylint: Pylint,
+  rubocop: RuboCop,
+  stylelint: Stylelint,
+  swiftlint: SwiftLint,
+  staticcheck: StaticCheck,
+  xo: XO,
+  tsc: TSC,
 
-	// Formatters (should be run after linters)
-	autopep8: Autopep8,
-	black: Black,
-	clang_format: ClangFormat,
-	dotnet_format: DotnetFormat,
-	gofmt: Gofmt,
-	oitnb: Oitnb,
-	rustfmt: RustFmt,
-	prettier: Prettier,
-	swift_format_lockwood: SwiftFormatLockwood,
-	swift_format_official: SwiftFormatOfficial,
+  // Formatters (should be run after linters)
+  autopep8: Autopep8,
+  black: Black,
+  clang_format: ClangFormat,
+  dotnet_format: DotnetFormat,
+  gofmt: Gofmt,
+  oitnb: Oitnb,
+  rustfmt: RustFmt,
+  prettier: Prettier,
+  swift_format_lockwood: SwiftFormatLockwood,
+  swift_format_official: SwiftFormatOfficial,
 
-	// Alias of `swift_format_lockwood` (for backward compatibility)
-	// TODO: Remove alias in v2
-	swiftformat: SwiftFormatLockwood,
+  // Alias of `swift_format_lockwood` (for backward compatibility)
+  // TODO: Remove alias in v2
+  swiftformat: SwiftFormatLockwood,
 };
 
 module.exports = linters;
